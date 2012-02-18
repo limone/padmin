e8785a7f9a8302fa1409a682a93bc1d5f00d07d6
Michael Laccetti <michael@laccetti.com>
2012-02-18 13:28:50 -0500

    * Removing node.js stuff from master

--- config.js
+++
@@ -1,8 +0,0 @@
-exports.getSiteConfig = function () {
-  configValues =  {
-    url: 'http://localhost:3000',
-    db: 'tcp://powerdns:powerdns2k10^^@plop:5433/powerdns',
-    secret: 'padmin2k12^^'
-  }
-  return configValues;
-}
--- routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-  require('./controllers/index.js')(app);
-  require('./controllers/acs.js')(app);
-  require('./controllers/domains.js')(app);
-};
